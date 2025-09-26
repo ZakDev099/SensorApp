df6167d830f8c6b506d404bf4bfeeca88b56ef98
Zak Dyer <30128282@tafe.wa.edu.au>
2025-09-26 14:02:34 +0800

    Updated Dataset
    added constructor, properties and function to set bounds

--- SensorApp/Classes/Dataset.cs
+++ SensorApp/Classes/Dataset.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SensorApp.classes
 {
-    public class Dataset
+    public class Dataset(string name, double[,] data)
     {
-        
+        public string Name = name;
+        public double[,] Data = data;
+        public double UpperBound;
+        public double LowerBound;
+
+        public void SetBounds(double upperBound, double lowerBound)
+        {
+            UpperBound = upperBound;
+            LowerBound = lowerBound;
+        }
     }
 }
