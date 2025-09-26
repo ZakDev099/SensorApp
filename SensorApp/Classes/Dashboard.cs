5ac641a837d44e6fa88b6aee75f97782eb32ded2
Zak Dyer <30128282@tafe.wa.edu.au>
2025-09-26 14:01:54 +0800

    Updated Dashboard
    Added basic functions to the dashboard

--- SensorApp/Classes/Dashboard.cs
+++ SensorApp/Classes/Dashboard.cs
@@ -8,6 +8,22 @@ namespace SensorApp.classes
 {
     public class Dashboard
     {
-        
+        public Dataset? ActiveDataset;
+        private int Position = 0;
+
+        public Dataset NextDataset()
+        {
+            return DataProcessing.Instance.Datasets[++Position];
+        }
+
+        public Dataset PreviousDataset()
+        {
+            return DataProcessing.Instance.Datasets[--Position];
+        }
+
+        public void SetActiveDataset(Dataset dataset)
+        {
+            ActiveDataset = dataset;
+        }
     }
 }
