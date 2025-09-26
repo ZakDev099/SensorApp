5c2635da7fa8ffb88ecf7993301339f299846f48
Zak Dyer <30128282@tafe.wa.edu.au>
2025-09-26 14:03:41 +0800

    Updated UserFeedback
    Added functions to show messages and changed class to static

--- SensorApp/Classes/UserFeedback.cs
+++ SensorApp/Classes/UserFeedback.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace SensorApp.classes
 {
-    public class UserFeedback
+    public static class UserFeedback
     {
-        
+        public static void ShowError(string message)
+        {
+            //Change after GUI implementation
+            Console.WriteLine($"ERROR: {message}");
+        }
+
+        public static void ShowInfo(string message)
+        {
+            //Change after GUI implementation
+            Console.WriteLine($"INFO: {message}");
+        }
     }
 }
