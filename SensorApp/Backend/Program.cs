9b8ccd72853265cd9cd78dec61023b8dfa45eb86
Zak Dyer <30128282@tafe.wa.edu.au>
2025-10-06 23:44:51 +0800

    Reformatted for readability

--- SensorApp/Backend/Program.cs
+++ SensorApp/Backend/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Double[,] sampleData = { { 1, 0 }, { 2, 1 }, { 3, 0 }, { 4, 9 } };
+        Double[,] sampleData = { { 1, 0, 1 }, { 2, 1, 4 }, { 3, 0, 6 }, { 4, 9, 1 } };
         Dataset dataset = new Dataset("MyDataset", sampleData);
         foreach (Double i in dataset.Data) {
             Console.WriteLine(i);
