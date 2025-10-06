353de247ce05c28d8f30a6ce5ddec9d2cff73dbf
Zak Dyer <30128282@tafe.wa.edu.au>
2025-10-06 17:59:40 +0800

    Several Changes and Reformatting
    - Integrated Color ResourceDictionary
    - Reformatted for readability
    - Repositioned elements and removed menu

--- SensorApp/Backend/Program.cs
+++ SensorApp/Backend/Program.cs
@@ -1,6 +1,16 @@
-﻿namespace SensorApp;
+﻿using SensorApp.classes;
+
+namespace SensorApp;
 
 public class Program
 {
-
+    static void Main(string[] args)
+    {
+        Double[,] sampleData = { { 1, 0 }, { 2, 1 }, { 3, 0 }, { 4, 9 } };
+        Dataset dataset = new Dataset("MyDataset", sampleData);
+        foreach (Double i in dataset.Data) {
+            Console.WriteLine(i);
+        }
+        Console.ReadLine();
+    }
 }
