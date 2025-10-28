0c6ff3751a6c160ec18d150e0e8c22645adf3ad7
Zak Dyer <30128282@tafe.wa.edu.au>
2025-10-28 23:07:02 +0800

    Added SortDataset tests

--- SensorAppTests/Utils/DataProcessingTests.cs
+++ SensorAppTests/Utils/DataProcessingTests.cs
@@ -3,6 +3,7 @@ using SensorApp.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -113,9 +114,9 @@ namespace SensorApp.Utils.Tests
             var result = DataProcessing.BinarySearch(sampleTarget, sampleData);
             bool targetsFound = true;
 
-            foreach (int address in expected) 
-            { 
-                if (result.Contains(address)) {}
+            foreach (int address in expected)
+            {
+                if (result.Contains(address)) { }
                 else
                 {
                     targetsFound = false;
@@ -177,6 +178,136 @@ namespace SensorApp.Utils.Tests
 
             var result = DataProcessing.BinarySearch(sampleTarget, sampleData);
             CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void SortDatasetTest_Typical()
+        {
+            double[][] sampleData = new double[10][]
+            {
+                [15.1, 2.5, 4.7, 9.1, 16.6],
+                [5.1, 8.6, 12.8, 88.1, 96.18],
+                [8.9, 12.6, 7.56, 81.4, 96.1],
+                [61.8, 12.71, 81, 14.56, 96.25],
+                [26.1, 27.5],
+                [5.1, 12.6, 55.21],
+                [],
+                [],
+                [],
+                []
+            };
+            var sampleDataSorted = DataProcessing.SortDataset(sampleData);
+
+            bool isSorted = true;
+            double previousDouble = sampleDataSorted[0].Item2;
+            foreach ((int, double) i in sampleDataSorted)
+            {
+                if (i.Item2 < previousDouble)
+                {
+                    isSorted = false; 
+                    break;
+                }
+                else
+                {
+                    previousDouble = i.Item2;
+                }
+            }
+
+            Assert.IsTrue(isSorted);
+        }
+
+        [TestMethod()]
+        public void SortDatasetTest_Edge()
+        {
+            double[][] sampleData = new double[10][]
+            {
+                [-1532444.1, 2245662.5, 44324674.7, -9983234.1, 165981234.6],
+                [595860982.1, 8938922.6, 1229384.8, -88324555.1, 967654543.18],
+                [81231244.9, -121245643.6, 712312324.56, 811233321.4, 96659864.1],
+                [61456546.8, 1212321.71, 812132144, 1455521.56, 961245564.25],
+                [26125453.1, -27123124.5],
+                [51424124.1, 1212314.6, 559818.21],
+                [-182141424.81, 18284912.9],
+                [882998123, 1282341.84, 81239541.23],
+                [],
+                []
+            };
+            var sampleDataSorted = DataProcessing.SortDataset(sampleData);
+
+            bool isSorted = true;
+            double previousDouble = sampleDataSorted[0].Item2;
+            foreach ((int, double) i in sampleDataSorted)
+            {
+                if (i.Item2 < previousDouble)
+                {
+                    isSorted = false;
+                    break;
+                }
+                else
+                {
+                    previousDouble = i.Item2;
+                }
+            }
+
+            Assert.IsTrue(isSorted);
+        }
+
+        [TestMethod()]
+        public void SortDatasetTest_Edge2()
+        {
+            double[][] sampleData = new double[10][]
+            {
+                [12.5, 12.5, 12.5],
+                [12.5, 12.5],
+                [],
+                [12.5],
+                [],
+                [12.5, 12.5],
+                [],
+                [12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5, 12.5],
+                [12.5, 12.5, 12.5],
+                [12.5]
+            };
+            var sampleDataSorted = DataProcessing.SortDataset(sampleData);
+
+            bool isSorted = true;
+            double previousDouble = sampleDataSorted[0].Item2;
+            foreach ((int, double) i in sampleDataSorted)
+            {
+                if (i.Item2 < previousDouble)
+                {
+                    isSorted = false;
+                    break;
+                }
+                else
+                {
+                    previousDouble = i.Item2;
+                }
+            }
+
+            Assert.IsTrue(isSorted);
+        }
+
+        [TestMethod()]
+        public void SortDatasetTest_Null()
+        {
+            double[][] sampleData = new double[10][]
+            {
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                []
+            };
+            var nullList = DataProcessing.SortDataset(sampleData);
+
+            // If no exception is thrown, test will reach this line.
+            Assert.IsTrue(true);
         }
     }
 }
